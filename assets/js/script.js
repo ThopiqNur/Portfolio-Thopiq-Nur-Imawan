84dede9cdd8856d980038b3bdf6957a5512fea11
THOPIQ NUR IMAWAN <134529433+ThopiqNur@users.noreply.github.com>
2024-12-26 01:14:06 +0700

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -221,7 +221,7 @@ srtop.reveal('.home .github', { interval: 800 });
 srtop.reveal('.home .twitter', { interval: 1000 });
 srtop.reveal('.home .telegram', { interval: 600 });
 srtop.reveal('.home .instagram', { interval: 600 });
-srtop.reveal('.home .dev', { interval: 600 });
+srtop.reveal('.home .whatshapp', { interval: 600 });
 
 /* SCROLL ABOUT */
 srtop.reveal('.about .content h3', { delay: 200 });
