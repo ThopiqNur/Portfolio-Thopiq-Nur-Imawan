977dac24b9d4136beb8e583f655bfbae410fbd66
THOPIQ NUR IMAWAN <134529433+ThopiqNur@users.noreply.github.com>
2024-12-26 21:27:27 +0700

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -249,6 +249,7 @@ srtop.reveal('.experience .timeline .container', { interval: 400 });
 srtop.reveal('.contact .container', { delay: 400 });
 srtop.reveal('.contact .container .form-group', { delay: 400 });
 
+/* Tab Bergerak */
 function updateTitle() {
     document.title = text.substring(index) + text.substring(0, index);
     index++;
